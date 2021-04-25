741d8bf29d2f18ea1ef3988510357a3c8961b3a2
rodrigo.monteiro <rodrigo.monteiro@kinghost.com.br>
2021-04-25 19:18:25 -0300

    Subindo estrutura de messages

---
+++ src/database/migrations/1619384687100-CreateMessages.ts
@@ -0,0 +1,56 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateMessages1619384687100 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "messages",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "admin_id",
+                        type: "uuid",
+                        isNullable: true
+                    },
+                    {
+                        name: "user_id",
+                        type: "uuid"
+                    },
+                    {
+                        name: "text",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: "FKUser",
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user_id"],
+                        onDelete: "SET NULL"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.dropTable("messages");
+    }
+
+}
