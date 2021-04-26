000251415455c94d3850c8574d7d2808b9ef14ce
rodrigo.monteiro <rodrigo.monteiro@kinghost.com.br>
2021-04-25 21:03:09 -0300

    Subindo estrutura de messages

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,17 +1,24 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
 import { UsersRepository } from "../repositories/UsersRepository";
 
 
 class UsersService{
+    private usersRepository: Repository<User>
+
+    constructor(){
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+
     async create(email:string){
-        const usersRepository =  getCustomRepository(UsersRepository);
-        const userAlreadyExists = await usersRepository.findOne({email})
+
+        const userAlreadyExists = await this.usersRepository.findOne({email})
         if(userAlreadyExists){
             return userAlreadyExists;
         }
-        const user = usersRepository.create({email});
+        const user = this.usersRepository.create({email});
 
-        await usersRepository.save(user);
+        await this.usersRepository.save(user);
 
         return user;
     }
